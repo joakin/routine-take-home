8d723c9b07f95d80a65287794fbf7bd3da082ef4
joakin <joaquin@chimeces.com>
2024-05-21 20:39:33 +0200

    s/App/Console/ and add some nice spacing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
-import App from "./App.vue";
+import Console from "./Console.vue";
 
-createApp(App).mount("#app");
+createApp(Console).mount("#app");
